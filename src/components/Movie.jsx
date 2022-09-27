7ce297a52c0d228364e8c238ee98f52dd6913be3
then0403 <39440926+then0403@users.noreply.github.com>
2022-09-27 08:52:04 -0700

    deploy commit

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import {UserAuth} from '../context/AuthContext';
+import {db} from '../firebase';
+import {arrayUnion, doc, updateDoc} from 'firebase/firestore';
 
 const Movie = ({item}) => {
     const [like, setLike] = useState(false);
+    const [saved, setSaved] = useState(false);
+    const {user} = UserAuth();
+
+    const movieID = doc(db, 'users', `${user?.email}`)
+
+    const savedShow = async () => {
+        if(user?.email) {
+            setLike(!like)
+            setSaved(true)
+            await updateDoc(movieID, {
+                savedShow: arrayUnion({
+                    id: item.id, 
+                    title: item.title, 
+                    img: item.backdrop_path
+                })
+            })
+        } else {
+            alert('Please log into account to save');
+        }
+    }
 
   return (
     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
@@ -15,12 +38,16 @@ const Movie = ({item}) => {
             <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
                 {item?.title}
             </p>
-            <p>
-                {like ? <FaHeart className='absolute top-4 left-4 text-gray-300' /> : <FaRegHeart className='absolute top-4 left-4 text-gray-300' />}
+            <p onClick={savedShow}>
+                {like ? ( 
+                    <FaHeart className='absolute top-4 left-4 text-gray-300' /> 
+                ):( 
+                    <FaRegHeart className='absolute top-4 left-4 text-gray-300' />
+                )}
             </p>
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Movie;
